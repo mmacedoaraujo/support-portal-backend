cf0a41a3436f18493abab2cd376f3659656d74ff
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-03 11:06:26 -0300

    implemented: more JWT validation related methods in JWTTokenProvider class

--- src/main/java/com/mmacedoaraujo/supportportal/utility/JWTTokenProvider.java
+++ src/main/java/com/mmacedoaraujo/supportportal/utility/JWTTokenProvider.java
@@ -4,23 +4,30 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.mmacedoaraujo.supportportal.constant.SecurityConstant;
 import com.mmacedoaraujo.supportportal.domain.UserPrincipal;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.auth0.jwt.algorithms.Algorithm.HMAC256;
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.mmacedoaraujo.supportportal.constant.SecurityConstant.*;
 import static java.time.Instant.now;
 import static java.util.Arrays.stream;
 
+@Component
 public class JWTTokenProvider {
+
     @Value("${jwt.secret}")
     private String secret;
 
@@ -41,6 +48,28 @@ public class JWTTokenProvider {
         return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null, authorities);
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return authenticationToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && isTokenExpired(verifier, token);
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
     private String[] getClaimsFromToken(String token) {
         JWTVerifier verifier = getJWTVerifier();
         return verifier.verify(token)
@@ -51,7 +80,7 @@ public class JWTTokenProvider {
     private JWTVerifier getJWTVerifier() {
         JWTVerifier verifier;
         try {
-            Algorithm  algorithm = Algorithm.HMAC512(secret);
+            Algorithm algorithm = Algorithm.HMAC512(secret);
             verifier = JWT.require(algorithm)
                     .withIssuer(MMACEDOARAUJO_LLC)
                     .build();
