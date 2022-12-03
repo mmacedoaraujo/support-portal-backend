bb91762d4b76c512987fb8170515a074814f3e69
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-03 09:55:40 -0300

    implemented: JWT related methods in JWTTokenProvider class

--- src/main/java/com/mmacedoaraujo/supportportal/utility/JWTTokenProvider.java
+++ src/main/java/com/mmacedoaraujo/supportportal/utility/JWTTokenProvider.java
@@ -1,30 +1,72 @@
 package com.mmacedoaraujo.supportportal.utility;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.mmacedoaraujo.supportportal.constant.SecurityConstant;
 import com.mmacedoaraujo.supportportal.domain.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC256;
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.mmacedoaraujo.supportportal.constant.SecurityConstant.*;
 import static java.time.Instant.now;
+import static java.util.Arrays.stream;
 
 public class JWTTokenProvider {
-    @Value("jwt.secret")
+    @Value("${jwt.secret}")
     private String secret;
 
     public String generateJwtToken(UserPrincipal userPrincipal) {
         String[] claims = getClaimsFromUser(userPrincipal);
         return JWT.create()
-                .withIssuer(SecurityConstant.MMACEDOARAUJO_LLC)
-                .withAudience(SecurityConstant.MMACEDOARAUJO_ADMINISTRATION)
+                .withIssuer(MMACEDOARAUJO_LLC)
+                .withAudience(MMACEDOARAUJO_ADMINISTRATION)
                 .withIssuedAt(now())
                 .withSubject(userPrincipal.getUsername())
-                .withArrayClaim(SecurityConstant.AUTHORITIES, claims)
-                .withExpiresAt(now().plusMillis(SecurityConstant.EXPIRATION_TIME))
-                .sign(Algorithm.HMAC512(secret.getBytes()));
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(now().plusMillis(EXPIRATION_TIME))
+                .sign(HMAC512(secret.getBytes()));
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token)
+                .getClaim(AUTHORITIES)
+                .asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm  algorithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer(MMACEDOARAUJO_LLC)
+                    .build();
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
     }
 
     private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : userPrincipal.getAuthorities()) {
+            authorities.add(grantedAuthority.getAuthority());
+        }
 
+        return authorities.toArray(new String[0]);
     }
 }
