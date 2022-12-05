5dd5ab0298df11eec222c7752ae8d10a4a6291cd
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-05 12:55:30 -0300

    created: JWTAccessDeniedHandler

--- src/main/java/com/mmacedoaraujo/supportportal/constant/SecurityConstant.java
+++ src/main/java/com/mmacedoaraujo/supportportal/constant/SecurityConstant.java
@@ -9,7 +9,7 @@ public class SecurityConstant {
     public static final String MMACEDOARAUJO_ADMINISTRATION = "User Management Portal";
     public static final String AUTHORITIES = "authorities";
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
-    public static final String ACCESS_DENIED = "You do not have permission to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
     public static final String[] PUBLIC_URLS = {"/user/login", "/user/register", "/user/resetpassword/**", "/user/image/**"};
 
---
+++ src/main/java/com/mmacedoaraujo/supportportal/filter/JWTAccessDeniedHandler.java
@@ -0,0 +1,37 @@
+package com.mmacedoaraujo.supportportal.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mmacedoaraujo.supportportal.domain.HttpResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.mmacedoaraujo.supportportal.constant.SecurityConstant.ACCESS_DENIED_MESSAGE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JWTAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException, ServletException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED)
+                .httpStatusCode(HttpStatus.UNAUTHORIZED.value())
+                .reason(HttpStatus.UNAUTHORIZED.getReasonPhrase().toUpperCase())
+                .message(ACCESS_DENIED_MESSAGE)
+                .build();
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
