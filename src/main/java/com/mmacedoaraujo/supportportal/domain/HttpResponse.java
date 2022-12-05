901c1202e25ea800a4967e519055ec76b5e97ebd
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-05 12:49:42 -0300

    created: JWTAuthenticationEntryPoint class

--- src/main/java/com/mmacedoaraujo/supportportal/domain/HttpResponse.java
+++ src/main/java/com/mmacedoaraujo/supportportal/domain/HttpResponse.java
@@ -1,15 +1,13 @@
 package com.mmacedoaraujo.supportportal.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class HttpResponse {
     private int httpStatusCode;
     private HttpStatus httpStatus;
---
+++ src/main/java/com/mmacedoaraujo/supportportal/filter/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.mmacedoaraujo.supportportal.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mmacedoaraujo.supportportal.domain.HttpResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.mmacedoaraujo.supportportal.constant.SecurityConstant.FORBIDDEN_MESSAGE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class JWTAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .httpStatus(HttpStatus.FORBIDDEN)
+                .httpStatusCode(HttpStatus.FORBIDDEN.value())
+                .reason(HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase())
+                .message(FORBIDDEN_MESSAGE)
+                .build();
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
