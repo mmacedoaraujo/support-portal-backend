ebc6edc085ef142febd625af683a3912f7786bf4
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-05 12:25:55 -0300

    implemented: all the required logic for the filter JWTAuthorizationFilter class

--- src/main/java/com/mmacedoaraujo/supportportal/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/mmacedoaraujo/supportportal/filter/JWTAuthorizationFilter.java
@@ -8,14 +8,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.mmacedoaraujo.supportportal.constant.SecurityConstant.OPTIONS_HTTP_METHOD;
 import static com.mmacedoaraujo.supportportal.constant.SecurityConstant.TOKEN_PREFIX;
 
 @AllArgsConstructor
+@Component
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     private final JWTTokenProvider jwtTokenProvider;
@@ -26,10 +32,20 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             response.setStatus(HttpStatus.OK.value());
         } else {
             String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-            if (authorizationHeader == null || authorizationHeader.startsWith(TOKEN_PREFIX)) {
+            if (authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_PREFIX)) {
                 filterChain.doFilter(request, response);
                 return;
             }
+            String token = authorizationHeader.substring(TOKEN_PREFIX.length());
+            String username = jwtTokenProvider.getSubject(token);
+            if (jwtTokenProvider.isTokenValid(username, token)) {
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                SecurityContextHolder.clearContext();
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
