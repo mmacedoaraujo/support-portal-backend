bbdc2be34595180e830bb30432e9d6e06d62da30
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-19 19:43:36 -0300

    created: EmailNotFoundException class

---
+++ src/main/java/com/mmacedoaraujo/supportportal/exception/domain/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mmacedoaraujo.supportportal.exception.domain;
+
+public class EmailNotFoundException extends Exception {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
