8d71b3d2d96a87c23cdbd41636ccce89b17942df
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-19 19:47:52 -0300

    created: UserNotFoundException

---
+++ src/main/java/com/mmacedoaraujo/supportportal/exception/domain/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mmacedoaraujo.supportportal.exception.domain;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
