026c894288f195088c57c75149becec44544d08b
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-19 19:46:13 -0300

    created: EmailExistException and UsernameExistException classes

---
+++ src/main/java/com/mmacedoaraujo/supportportal/exception/domain/UsernameExistException.java
@@ -0,0 +1,7 @@
+package com.mmacedoaraujo.supportportal.exception.domain;
+
+public class UsernameExistException extends Exception {
+    public UsernameExistException(String message) {
+        super(message);
+    }
+}
