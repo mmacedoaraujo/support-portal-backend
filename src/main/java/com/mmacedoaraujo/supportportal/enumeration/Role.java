081a2d98f7fdf55a3b58fd50479804f926c6de60
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-20 19:28:01 -0300

    created: Role class

---
+++ src/main/java/com/mmacedoaraujo/supportportal/enumeration/Role.java
@@ -0,0 +1,22 @@
+package com.mmacedoaraujo.supportportal.enumeration;
+
+import static com.mmacedoaraujo.supportportal.constant.Authority.*;
+
+public enum Role {
+
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_USER(SUPER_USER_AUTHORITIES);
+
+    private String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+}
