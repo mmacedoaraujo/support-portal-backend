6af840e2e1c4e1310b779fe3b9b8ee25133857fb
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-20 13:39:35 -0300

    override: whitelabel page for the actual error

--- src/main/java/com/mmacedoaraujo/supportportal/exception/domain/ExceptionHandling.java
+++ src/main/java/com/mmacedoaraujo/supportportal/exception/domain/ExceptionHandling.java
@@ -3,6 +3,7 @@ package com.mmacedoaraujo.supportportal.exception.domain;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.mmacedoaraujo.supportportal.domain.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.persistence.NoResultException;
 import java.io.IOException;
@@ -21,7 +24,8 @@ import java.util.Objects;
 
 @RestControllerAdvice
 @Slf4j
-public class ExceptionHandling {
+@EnableWebMvc
+public class ExceptionHandling implements ErrorController {
 
     private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration";
     private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
@@ -30,6 +34,7 @@ public class ExceptionHandling {
     private static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact administration";
     private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
     private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    public static final String ERROR_PATH = "/error";
 
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<HttpResponse> accountDisabledException() {
@@ -76,6 +81,11 @@ public class ExceptionHandling {
         return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
+//    @ExceptionHandler(NoHandlerFoundException.class)
+//    public ResponseEntity<HttpResponse> noHandlerFoundException() {
+//        return createHttpResponse(HttpStatus.BAD_REQUEST, "This page was not found");
+//    }
+
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<HttpResponse> methodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
         HttpMethod supportedMethod = Objects.requireNonNull(exception.getSupportedHttpMethods()).iterator().next();
@@ -104,5 +114,10 @@ public class ExceptionHandling {
 
         return new ResponseEntity<>(new HttpResponse(httpStatus.value(), new Date(), httpStatus,
                 httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase()), httpStatus);
+    }
+
+    @RequestMapping(ERROR_PATH)
+    public ResponseEntity<HttpResponse> notFound404() {
+        return createHttpResponse(HttpStatus.NOT_FOUND, "There is no mapping for this URL");
     }
 }
--- src/main/java/com/mmacedoaraujo/supportportal/resource/UserResource.java
+++ src/main/java/com/mmacedoaraujo/supportportal/resource/UserResource.java
@@ -1,16 +1,17 @@
 package com.mmacedoaraujo.supportportal.resource;
 
 import com.mmacedoaraujo.supportportal.exception.domain.ExceptionHandling;
+import com.mmacedoaraujo.supportportal.exception.domain.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(path = {"/", "/users"})
 public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
-    public String showUser() {
-        return "application works";
+    public String showUser() throws UserNotFoundException {
+        throw new UserNotFoundException("The user is not found");
     }
 }
