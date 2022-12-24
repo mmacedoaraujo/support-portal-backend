41f6610caccab08f100d18b80078e7b9b821bd07
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-23 21:13:25 -0300

    implemented: constructor on LoginAttemptService class

--- src/main/java/com/mmacedoaraujo/supportportal/service/LoginAttemptService.java
+++ src/main/java/com/mmacedoaraujo/supportportal/service/LoginAttemptService.java
@@ -1,10 +1,29 @@
 package com.mmacedoaraujo.supportportal.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoginAttemptService {
 
     public static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
     public static final int ATTEMPT_INCREMENT = 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    public LoginAttemptService() {
+        super();
+        loginAttemptCache = CacheBuilder
+                .newBuilder()
+                .expireAfterWrite(15, TimeUnit.MINUTES)
+                .maximumSize(100).build(new CacheLoader<String, Integer>() {
+                    @Override
+                    public Integer load(String key) throws Exception {
+                        return 0;
+                    }
+                });
+    }
 }
