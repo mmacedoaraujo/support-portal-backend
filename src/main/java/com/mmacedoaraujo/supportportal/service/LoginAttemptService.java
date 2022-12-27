2e1404d4807c2c5310d522afe316541d6b7b080a
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-26 22:44:55 -0300

    implemented: more methods anti brute force class

--- src/main/java/com/mmacedoaraujo/supportportal/service/LoginAttemptService.java
+++ src/main/java/com/mmacedoaraujo/supportportal/service/LoginAttemptService.java
@@ -3,8 +3,10 @@ package com.mmacedoaraujo.supportportal.service;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.mmacedoaraujo.supportportal.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -13,8 +15,9 @@ public class LoginAttemptService {
     public static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
     public static final int ATTEMPT_INCREMENT = 1;
     private LoadingCache<String, Integer> loginAttemptCache;
+    private final UserRepository userRepository;
 
-    public LoginAttemptService() {
+    public LoginAttemptService(UserRepository userRepository) {
         super();
         loginAttemptCache = CacheBuilder
                 .newBuilder()
@@ -25,5 +28,22 @@ public class LoginAttemptService {
                         return 0;
                     }
                 });
+        this.userRepository = userRepository;
+    }
+
+    public void evictUserFromLoginAttemptCache(String username) {
+        loginAttemptCache.invalidate(username);
+    }
+
+    public void addUserToLoginAttemptCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
+        loginAttemptCache.put(username, attempts);
+    }
+
+    public boolean exceededMaxAttempt(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
     }
 }
+
+
