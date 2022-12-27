90896a0e1d5a77d3682b5de4016ab50257b34e09
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-26 22:53:10 -0300

    created: AuthenticationFailureListener

---
+++ src/main/java/com/mmacedoaraujo/supportportal/listener/AuthenticationFailureListener.java
@@ -0,0 +1,25 @@
+package com.mmacedoaraujo.supportportal.listener;
+
+import com.mmacedoaraujo.supportportal.service.LoginAttemptService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+@AllArgsConstructor
+public class AuthenticationFailureListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) throws ExecutionException {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof String) {
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+    }
+}
