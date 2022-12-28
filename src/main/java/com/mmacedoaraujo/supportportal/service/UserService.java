aa111026fad72e22b8edfa0a7eba6ddbfd250998
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-28 20:52:09 -0300

    added: new methods at UserService interface

--- src/main/java/com/mmacedoaraujo/supportportal/service/UserService.java
+++ src/main/java/com/mmacedoaraujo/supportportal/service/UserService.java
@@ -4,6 +4,7 @@ import com.mmacedoaraujo.supportportal.domain.User;
 import com.mmacedoaraujo.supportportal.exception.domain.EmailExistException;
 import com.mmacedoaraujo.supportportal.exception.domain.UserNotFoundException;
 import com.mmacedoaraujo.supportportal.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
@@ -17,4 +18,14 @@ public interface UserService {
     User findByUsername(String username);
 
     User findByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String username, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(Long id);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 }
