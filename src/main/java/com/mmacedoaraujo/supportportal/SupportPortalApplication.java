305b51f07c613ab6a7cccc9d8704516dab1cdc9f
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-12-29 10:37:41 -0300

    created: FileConstant class

--- src/main/java/com/mmacedoaraujo/supportportal/SupportPortalApplication.java
+++ src/main/java/com/mmacedoaraujo/supportportal/SupportPortalApplication.java
@@ -3,11 +3,14 @@ package com.mmacedoaraujo.supportportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.mmacedoaraujo.supportportal.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class SupportportalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SupportportalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SupportportalApplication.class, args);
+        new File(USER_FOLDER).mkdirs();
+    }
 
 }
---
+++ src/main/java/com/mmacedoaraujo/supportportal/constant/FileConstant.java
@@ -0,0 +1,16 @@
+package com.mmacedoaraujo.supportportal.constant;
+
+public class FileConstant {
+
+    public static final String USER_IMAGE_PATH = "/user/image/";
+    public static final String JPEG_EXTENSION = "jpg";
+    public static final String USER_FOLDER = System.getProperty("user.home") + "/supporportal/user/";
+    public static final String DIRECTORY_CRETED = "Created directory for: ";
+    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile/";
+    public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
+    public static final String DOT = ".";
+    public static final String FORWARD_SLASH = "/";
+    public static final String TEMP_PROFILE_IMAGE_FIRST_PART_URL = "https://source.boringavatars.com/beam/120/";
+    public static final String TEMP_PROFILE_IMAGE_SECOND_PART_URL = "?colors=4C3D31,F18273,F2BD76,F4F5DE,C4CEB0";
+
+}
