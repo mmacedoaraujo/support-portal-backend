e5614f102d335e55e303f7780a8ff1cc37040938
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-02 15:12:51 -0300

    added: password and email are hidden now on response at login

--- src/main/java/com/mmacedoaraujo/supportportal/domain/User.java
+++ src/main/java/com/mmacedoaraujo/supportportal/domain/User.java
@@ -1,5 +1,6 @@
 package com.mmacedoaraujo.supportportal.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.hibernate.Hibernate;
 
@@ -25,7 +26,9 @@ public class User implements Serializable {
     private String firstName;
     private String lastName;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String email;
     private String profileImageUrl;
     private Date lastLoginDate;
