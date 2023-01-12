fa59bb6160279bd77ecdf9fb73f44f6c4f496dfc
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2023-01-12 12:21:10 -0300

    changed: cors configuration to expose headers to the frontend

--- src/main/java/com/mmacedoaraujo/supportportal/SupportPortalApplication.java
+++ src/main/java/com/mmacedoaraujo/supportportal/SupportPortalApplication.java
@@ -2,8 +2,14 @@ package com.mmacedoaraujo.supportportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static com.mmacedoaraujo.supportportal.constant.FileConstant.USER_FOLDER;
 
@@ -14,5 +20,23 @@ public class SupportportalApplication {
         SpringApplication.run(SupportportalApplication.class, args);
         new File(USER_FOLDER).mkdirs();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+
+    }
+
 
 }
